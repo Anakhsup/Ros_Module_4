b455494251fc0c66e2065654e6d84d9e51917cde
Anakhsup <145113685+Anakhsup@users.noreply.github.com>
2024-11-10 20:12:14 +0700

    Add files via upload
    colcon build --packages-select learning_tf2_py
    -------------------------------------------------------------------------
    . install/setup.bash
    ros2 launch learning_tf2_py turtle_tf2.launch.py
    -------------------------------------------------------------------------
    ros2 run turtlesim turtle_teleop_key
    -------------------------------------------------------------------------
    ros2 run rviz2 rviz2 -d $(ros2 pkg prefix --share turtle_tf2_py)/rviz/turtle_rviz.rviz

---
+++ ex02/learning_tf2_py/launch/turtle_tf2_dynamic_frame_demo.launch.py
@@ -0,0 +1,68 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    demo_nodes = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('learning_tf2_py'), 'launch'),
+            '/turtle_tf2_demo.launch.py']),
+       launch_arguments={'target_frame': 'carrot1'}.items(),
+       )
+       
+    return LaunchDescription([
+        Node(
+            package='turtlesim',
+            executable='turtlesim_node',
+            name='sim'
+        ),
+        Node(
+            package='learning_tf2_py',
+            executable='turtle_tf2_broadcaster',
+            name='broadcaster1',
+            parameters=[
+                {'turtlename': 'turtle1'}
+            ]
+        ),
+        DeclareLaunchArgument(
+            'target_frame', default_value='turtle1',
+            description='Target frame name.'
+        ),
+        Node(
+            package='learning_tf2_py',
+            executable='turtle_tf2_broadcaster',
+            name='broadcaster2',
+            parameters=[
+                {'turtlename': 'turtle2'}
+            ]
+        ),
+        Node(
+            package='learning_tf2_py',
+            executable='turtle_tf2_listener',
+            name='listener',
+            parameters=[
+                {'target_frame': 'carrot1'}
+            ]
+        ),
+        Node(
+            package='learning_tf2_py',
+            executable='dynamic_frame_tf2_broadcaster',
+            name='dynamic_broadcaster',
+            parameters=[
+                {'radius': 2},
+                {'direction_of_rotation': 1}
+            ]
+        ),
+    ])
+    
+    
+    
+    
